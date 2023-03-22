1c1964b64b00aed09e2867b475654de5d7cc698e
unkutony <scottopoke4@gmail.com>
2023-03-22 10:13:05 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+/*
+ * File: 101-natural.c
+ * Auth: vincent kip
+ */
+
+#include <stdio.h>
+
+/**
+ * main - List all the natural numbers belo (excluded)
+ * that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
