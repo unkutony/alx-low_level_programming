540095bf31825ccf93c2d1dd89e6e176c4776fae
unkutony <scottopoke4@gmail.com>
2023-03-22 12:21:24 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,9 +8,9 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if ((n >= 0); i <= 15)
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; && i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
